0e1307ad50a5dcb160602765b4008adc1b8ff466
bertbertcity <robertl@udel.edu>
2016-05-05 00:25:08 +0200

    split up mod5prefab

---
+++ SpawnStats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnStats : MonoBehaviour {
+	public int currModule;
+	public int currPart;
+	// Use this for initialization
+	public void Start () {
+		currModule = 0;
+		currPart = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void updatePart() {
+		currPart = (currPart + 1) % 4;
+	}
+}
