e1018b9b83b5eaf2ecc2475eba7a0b808dff19a7
emc277 <emc277@cornell.edu>
2016-05-04 22:25:47 +0200

    Added files via upload

---
+++ helpscript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class helpscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NextLevel(){
+		Application.LoadLevel ("help");
+		Debug.Log("Next scene");
+	}
+}
